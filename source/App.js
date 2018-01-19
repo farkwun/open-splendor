162380da9ac335435e32b644201437dbc7e31c1c
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-19 17:23:50 -0500

    Removing commented data

--- source/App.js
+++ source/App.js
@@ -32,16 +32,6 @@ let nobles = [
     ],
     img_url: "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
   },
-  /*{
-    id: 1,
-    prestige: 3,
-    costs: {
-      "green": 3,
-      "blue": 3
-      "white": 3
-    },
-    img: "https://images.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png"
-  }*/
 ];
 
 React.render(<GameBoard nobles={nobles}/>, document.getElementById('root'));
