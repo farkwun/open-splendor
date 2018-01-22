3ac7670fc3c24575e95614ef34a2ba2a0865b21d
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-22 15:29:27 -0500

    Adding MockData file

---
+++ source/MockData.js
@@ -0,0 +1,77 @@
+export const nobles = [
+  {
+    id: 1,
+    prestige: 3,
+    costs: [
+      {
+        type: "red",
+        val: 4
+      },
+      {
+        type: "green",
+        val: 4
+      },
+    ],
+    img_url: "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  {
+    id: 2,
+    prestige: 3,
+    costs: [
+      {
+        type: "black",
+        val: 4
+      },
+      {
+        type: "red",
+        val: 4
+      },
+    ],
+    img_url: "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  {
+    id: 3,
+    prestige: 3,
+    costs: [
+      {
+        type: "red",
+        val: 4
+      },
+      {
+        type: "green",
+        val: 4
+      },
+    ],
+    img_url: "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  {
+    id: 4,
+    prestige: 3,
+    costs: [
+      {
+        type: "red",
+        val: 4
+      },
+      {
+        type: "green",
+        val: 4
+      },
+    ],
+    img_url: "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  {
+    id: 5,
+    prestige: 3,
+    costs: [
+      {
+        type: "red",
+        val: 4
+      },
+      {
+        type: "green",
+        val: 4
+      },
+    ],
+    img_url: "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+];
