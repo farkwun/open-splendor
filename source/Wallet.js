c44e493a06f4c5dc656bbfedd82054b316c35b87
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-23 18:40:54 -0500

    Adding Wallet

--- source/Wallet.js
+++ source/Wallet.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
+    let coins = this.props.coins.map((coin) => {
+      return (
+        <div className="counter" style={{backgroundColor: coin.type}}>
+          {coin.amount}
+        </div>
+      );
+    });
     return (
-      <h1>Wallet</h1>
+      <div className="wallet">
+        <h3 className="box__header">Wallet</h3>
+        {coins}
+      </div>
     );
   }
 }
