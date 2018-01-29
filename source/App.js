6320c6ff3535e1d730085bfda97b0dc94213a3da
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-29 15:13:02 -0500

    Placing data into state within App component

--- source/App.js
+++ source/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 import GameBoard from './GameBoard';
+import Info from './Info';
 
 import * as mock from './MockData';
 
-React.render(<GameBoard nobles={mock.nobles} levels={mock.levels} coins={mock.coins} players={mock.players}/>, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      nobles: mock.nobles,
+      levels: mock.levels,
+      coins: mock.coins,
+      current_player: mock.current_player,
+      players: mock.players
+    };
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <GameBoard nobles={this.state.nobles}
+          levels={this.state.levels}
+          coins={this.state.coins}
+          players={this.state.players}
+        />
+        <Info state={this.state}/>
+      </div>)
+  }
+}
+
+React.render(<App />, document.getElementById('root'));
