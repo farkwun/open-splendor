47528d4b83f5724ddb8551af4222ecd6c9262f1a
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-29 18:34:40 -0500

    Prototyping play order

--- source/App.js
+++ source/App.js
@@ -12,21 +12,38 @@ class App extends React.Component {
       nobles: mock.nobles,
       levels: mock.levels,
       coins: mock.coins,
-      current_player: mock.current_player,
       round_num: mock.round_num,
+      play_order: mock.play_order,
+      play_index: mock.play_index,
       players: mock.players
     };
   }
 
+  incrementPlayIndex() {
+    let index = this.state.play_index + 1;
+    if (index >= this.state.play_order.length){
+      index = 0;
+    }
+    this.setState({
+      play_index: index
+    });
+  }
+
   render () {
+    let players = this.state.play_order.map((player_id) => {
+      return this.state.players[player_id];
+    });
+
     return (
       <div className="app">
         <GameBoard nobles={this.state.nobles}
           levels={this.state.levels}
           coins={this.state.coins}
-          players={this.state.players}
+          players={players}
         />
-        <Info state={this.state}/>
+        <Info state={this.state} onClick={() => {
+          this.incrementPlayIndex();
+        }}/>
       </div>)
   }
 }
--- source/Info.js
+++ source/Info.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 class Info extends React.Component {
   render() {
-    let current_player_prompt = "The current player is [" + this.props.state.current_player + "]";
+    let current_player = this.props.state.play_order[this.props.state.play_index];
+    let current_player_prompt = "The current player is [" + current_player + "]";
     let round_prompt = "It is currently Round " + this.props.state.round_num;
     return (
-      <div className="info">
+      <div className="info" onClick={this.props.onClick}>
         <div>{current_player_prompt}</div>
         <div>{round_prompt}</div>
       </div>
