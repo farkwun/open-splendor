e90de6bc1a98fab027e2ad42fa4758b3abc6fb49
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-30 13:11:26 -0500

    Adding Cost component

---
+++ source/Cost.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Cost extends React.Component {
+  render() {
+    let type = this.props.type;
+    let val = this.props.val;
+    let bonus = 0;
+
+    let style = {
+      backgroundColor: type
+    };
+
+    if (this.props.getBonus) {
+      bonus = this.props.getBonus(type);
+    }
+
+    style.color = "white";
+    if (bonus) {
+      style.color = "#00ff00";
+    }
+
+    val = val - bonus;
+
+    return (
+      <div className="cost" style={style}>
+        {val}
+      </div>
+    );
+  }
+}
+
+export default Cost;
