040b66526cdc19d3191d7a905e7cb6fb31c9b6f7
Bryan Chu <thisisbryanchu@gmail.com>
2018-02-16 16:03:30 -0500

    Moving store and dispatch logic to GameBoard

--- source/components/App.js
+++ source/components/App.js
@@ -1,123 +1,15 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 
 import GameBoard from "./GameBoard";
 
-import {
-  addCoinToStash,
-  removeCoinFromStash,
-  resetStash,
-  takeCoinsFromStash,
-  buyCard
-} from "../redux/modules/shared";
-
-import { setPlayIndex } from "../redux/modules/playIndex";
-import { setRoundNum } from "../redux/modules/roundNum";
-
-import { cards } from "../data/static";
-
 class App extends Component {
-  getCurrPlayer = () =>
-    this.props.players[this.props.playOrder[this.props.playIndex]];
-
-  getBonus = type =>
-    this.getCurrPlayer().cards.filter(cardId => cards[cardId].type === type)
-      .length;
-
-  buyCard = (cardId, levelId) => {
-    this.props.buyCardFor(cardId, this.getCurrPlayer(), levelId);
-    this.incrementPlayIndex();
-  };
-
-  addToStash = type => {
-    if (this.props.coins[type] > 0 && this.props.stash.length < 3) {
-      this.props.addToStash(type);
-    }
-  };
-
-  takeStash = () => {
-    const currPlayerId = this.props.playOrder[this.props.playIndex];
-    this.props.takeStash(currPlayerId, this.props.stash);
-    this.incrementPlayIndex();
-  };
-
-  incrementPlayIndex = () => {
-    let index = this.props.playIndex + 1;
-    let roundNum = this.props.roundNum;
-
-    if (index >= this.props.playOrder.length) {
-      index = 0;
-      roundNum = roundNum + 1;
-    }
-
-    this.props.setCurrPlayer(index);
-    this.props.setRound(roundNum);
-  };
-
   render() {
-    const player = this.getCurrPlayer();
-
     return (
       <div className="app">
-        <GameBoard
-          nobleList={this.props.nobleList}
-          levels={this.props.levels}
-          coins={this.props.coins}
-          stash={this.props.stash}
-          players={this.props.players}
-          playOrder={this.props.playOrder}
-          getBonus={this.getBonus}
-          currPlayer={player}
-          buyCard={this.buyCard}
-          removeFromStash={this.props.removeFromStash}
-          addToStash={this.addToStash}
-          takeStash={this.takeStash}
-          clearStash={this.props.clearStash}
-          roundNum={this.props.roundNum}
-          incrementIndex={this.incrementPlayIndex}
-        />
+        <GameBoard />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    nobleList: state.nobleList,
-    levels: state.levels,
-    coins: state.coins,
-    roundNum: state.roundNum,
-    stash: state.stash,
-    playOrder: state.playOrder,
-    playIndex: state.playIndex,
-    players: state.players
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    addToStash: type => {
-      dispatch(addCoinToStash(type));
-    },
-    removeFromStash: (type, index) => {
-      dispatch(removeCoinFromStash(type, index));
-    },
-    clearStash: stash => {
-      dispatch(resetStash(stash));
-    },
-    takeStash: (playerId, stash) => {
-      dispatch(takeCoinsFromStash(playerId, stash));
-    },
-    setCurrPlayer: index => {
-      dispatch(setPlayIndex(index));
-    },
-    setRound: num => {
-      dispatch(setRoundNum(num));
-    },
-    buyCardFor: (card, player, levelId) => {
-      dispatch(buyCard(card, player, levelId));
-    }
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- source/components/GameBoard.js
+++ source/components/GameBoard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import NobleList from "./NobleList";
@@ -8,26 +9,75 @@ import Stash from "./Stash";
 import PlayerBoard from "./PlayerBoard";
 import Info from "./Info";
 
+import {
+  addCoinToStash,
+  removeCoinFromStash,
+  resetStash,
+  takeCoinsFromStash,
+  buyCard
+} from "../redux/modules/shared";
+
+import { setPlayIndex } from "../redux/modules/playIndex";
+import { setRoundNum } from "../redux/modules/roundNum";
+
+import { cards } from "../data/static";
+
 class GameBoard extends Component {
+  getCurrPlayer = () =>
+    this.props.players[this.props.playOrder[this.props.playIndex]];
+
+  getBonus = type =>
+    this.getCurrPlayer().cards.filter(cardId => cards[cardId].type === type)
+      .length;
+
+  buyCard = (cardId, levelId) => {
+    this.props.buyCardFor(cardId, this.getCurrPlayer(), levelId);
+    this.incrementPlayIndex();
+  };
+
+  addToStash = type => {
+    if (this.props.coins[type] > 0 && this.props.stash.length < 3) {
+      this.props.addToStash(type);
+    }
+  };
+
+  takeStash = () => {
+    const currPlayerId = this.props.playOrder[this.props.playIndex];
+    this.props.takeStash(currPlayerId, this.props.stash);
+    this.incrementPlayIndex();
+  };
+
+  incrementPlayIndex = () => {
+    let index = this.props.playIndex + 1;
+    let roundNum = this.props.roundNum;
+
+    if (index >= this.props.playOrder.length) {
+      index = 0;
+      roundNum = roundNum + 1;
+    }
+
+    this.props.setCurrPlayer(index);
+    this.props.setRound(roundNum);
+  };
+
   render() {
+    const player = this.getCurrPlayer();
+
     return (
       <div className="game__board">
         <NobleList nobleList={this.props.nobleList} />
         <LevelBoard
           levels={this.props.levels}
-          buyCard={this.props.buyCard}
-          getBonus={this.props.getBonus}
-          currPlayer={this.props.currPlayer}
+          buyCard={this.buyCard}
+          getBonus={this.getBonus}
+          currPlayer={player}
         />
-        <CoinBoard
-          coins={this.props.coins}
-          addToStash={this.props.addToStash}
-        />
+        <CoinBoard coins={this.props.coins} addToStash={this.addToStash} />
         <Stash
           stash={this.props.stash}
-          currPlayer={this.props.currPlayer}
+          currPlayer={player}
           removeFromStash={this.props.removeFromStash}
-          takeStash={this.props.takeStash}
+          takeStash={this.takeStash}
           clearStash={this.props.clearStash}
         />
         <PlayerBoard
@@ -35,9 +85,9 @@ class GameBoard extends Component {
           players={this.props.players}
         />
         <Info
-          currPlayer={this.props.currPlayer}
+          currPlayer={player}
           roundNum={this.props.roundNum}
-          incrementIndex={this.props.incrementIndex}
+          incrementIndex={this.incrementIndex}
         />
       </div>
     );
@@ -45,6 +95,7 @@ class GameBoard extends Component {
 }
 
 GameBoard.propTypes = {
+  playIndex: PropTypes.number.isRequired,
   roundNum: PropTypes.number.isRequired,
   nobleList: PropTypes.array.isRequired,
   levels: PropTypes.array.isRequired,
@@ -53,15 +104,53 @@ GameBoard.propTypes = {
 
   players: PropTypes.object.isRequired,
   coins: PropTypes.object.isRequired,
-  currPlayer: PropTypes.object.isRequired,
 
-  buyCard: PropTypes.func.isRequired,
-  getBonus: PropTypes.func.isRequired,
+  buyCardFor: PropTypes.func.isRequired,
   addToStash: PropTypes.func.isRequired,
-  removeFromStash: PropTypes.func.isRequired,
   takeStash: PropTypes.func.isRequired,
-  clearStash: PropTypes.func.isRequired,
-  incrementIndex: PropTypes.func.isRequired
+  setCurrPlayer: PropTypes.func.isRequired,
+  setRound: PropTypes.func.isRequired,
+  removeFromStash: PropTypes.func.isRequired,
+  clearStash: PropTypes.func.isRequired
 };
 
-export default GameBoard;
+function mapStateToProps(state) {
+  return {
+    nobleList: state.nobleList,
+    levels: state.levels,
+    coins: state.coins,
+    roundNum: state.roundNum,
+    stash: state.stash,
+    playOrder: state.playOrder,
+    playIndex: state.playIndex,
+    players: state.players
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToStash: type => {
+      dispatch(addCoinToStash(type));
+    },
+    removeFromStash: (type, index) => {
+      dispatch(removeCoinFromStash(type, index));
+    },
+    clearStash: stash => {
+      dispatch(resetStash(stash));
+    },
+    takeStash: (playerId, stash) => {
+      dispatch(takeCoinsFromStash(playerId, stash));
+    },
+    setCurrPlayer: index => {
+      dispatch(setPlayIndex(index));
+    },
+    setRound: num => {
+      dispatch(setRoundNum(num));
+    },
+    buyCardFor: (card, player, levelId) => {
+      dispatch(buyCard(card, player, levelId));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameBoard);
