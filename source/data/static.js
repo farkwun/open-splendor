b2399b141b095fe1053a1dae089cdc7842406ce6
Bryan Chu <thisisbryanchu@gmail.com>
2018-03-03 03:55:39 -0500

    Adding real card and noble data to static.js

--- source/data/static.js
+++ source/data/static.js
@@ -1,109 +1,910 @@
 export const cards = {
-  1: {
-    id: "1",
+  rdDrBpz: {
+    id: "rdDrBpz",
     prestige: 0,
-    type: "blue",
+    type: "white",
     costs: {
-      red: 1,
-      green: 1,
+      black: 2,
+      blue: 2
+    }
+  },
+  NGxSCgc: {
+    id: "NGxSCgc",
+    prestige: 1,
+    type: "white",
+    costs: {
+      green: 4
+    }
+  },
+  dYcnZeJ: {
+    id: "dYcnZeJ",
+    prestige: 0,
+    type: "white",
+    costs: {
+      black: 1,
+      white: 3,
       blue: 1
     }
   },
-  2: {
-    id: "2",
+  WBRDKkg: {
+    id: "WBRDKkg",
     prestige: 0,
-    type: "blue",
+    type: "white",
     costs: {
-      red: 1,
+      black: 1,
+      blue: 2,
+      green: 2
+    }
+  },
+  wIliHIk: {
+    id: "wIliHIk",
+    prestige: 0,
+    type: "white",
+    costs: {
+      black: 1,
+      blue: 1,
+      green: 2,
+      red: 1
+    }
+  },
+  ZquycMo: {
+    id: "ZquycMo",
+    prestige: 0,
+    type: "white",
+    costs: {
+      black: 1,
+      red: 2
+    }
+  },
+  akmIPkz: {
+    id: "akmIPkz",
+    prestige: 0,
+    type: "white",
+    costs: {
+      blue: 3
+    }
+  },
+  YTpbfvJ: {
+    id: "YTpbfvJ",
+    prestige: 0,
+    type: "white",
+    costs: {
+      black: 1,
+      blue: 1,
       green: 1,
+      red: 1
+    }
+  },
+  qoFnqSf: {
+    id: "qoFnqSf",
+    prestige: 0,
+    type: "green",
+    costs: {
+      black: 2,
+      white: 1,
+      blue: 1,
+      red: 1
+    }
+  },
+  QicWptH: {
+    id: "QicWptH",
+    prestige: 0,
+    type: "green",
+    costs: {
+      white: 2,
       blue: 1
     }
   },
-  3: {
-    id: "3",
+  xvgZcpy: {
+    id: "xvgZcpy",
     prestige: 0,
-    type: "blue",
+    type: "green",
     costs: {
-      red: 1,
-      green: 1,
-      blue: 1
+      red: 3
     }
   },
-  4: {
-    id: "4",
+  zszJfmv: {
+    id: "zszJfmv",
+    prestige: 0,
+    type: "green",
+    costs: {
+      blue: 2,
+      red: 2
+    }
+  },
+  aGJuUEb: {
+    id: "aGJuUEb",
+    prestige: 0,
+    type: "green",
+    costs: {
+      black: 2,
+      blue: 1,
+      red: 2
+    }
+  },
+  HCtKKTQ: {
+    id: "HCtKKTQ",
+    prestige: 0,
+    type: "green",
+    costs: {
+      white: 1,
+      blue: 3,
+      green: 1
+    }
+  },
+  SScRZdw: {
+    id: "SScRZdw",
     prestige: 1,
     type: "green",
     costs: {
-      red: 2,
-      green: 2,
-      blue: 2
+      black: 4
     }
   },
-  5: {
-    id: "5",
-    prestige: 1,
-    type: "green",
-    costs: {
-      red: 2,
-      green: 2,
-      blue: 2
-    }
-  },
-  6: {
-    id: "6",
-    prestige: 1,
-    type: "green",
-    costs: {
-      red: 2,
-      green: 2,
-      blue: 2
-    }
-  },
-  7: {
-    id: "7",
-    prestige: 2,
-    type: "red",
-    costs: {
-      red: 2,
-      green: 2,
-      blue: 2
-    }
-  },
-  8: {
-    id: "8",
-    prestige: 2,
-    type: "red",
-    costs: {
-      red: 2,
-      green: 2,
-      blue: 2
-    }
-  },
-  9: {
-    id: "9",
-    prestige: 2,
-    type: "red",
-    costs: {
-      red: 2,
-      green: 2,
-      blue: 2
-    }
-  },
-  10: {
-    id: "10",
+  LPxwzFZ: {
+    id: "LPxwzFZ",
     prestige: 0,
     type: "green",
     costs: {
-      red: 1,
+      black: 1,
+      white: 1,
+      blue: 1,
+      red: 1
+    }
+  },
+  pAZuWzR: {
+    id: "pAZuWzR",
+    prestige: 0,
+    type: "red",
+    costs: {
+      blue: 2,
+      green: 1
+    }
+  },
+  oWoJttz: {
+    id: "oWoJttz",
+    prestige: 1,
+    type: "red",
+    costs: {
+      white: 4
+    }
+  },
+  gVuTwAY: {
+    id: "gVuTwAY",
+    prestige: 0,
+    type: "red",
+    costs: {
+      black: 1,
+      white: 1,
+      blue: 1,
+      green: 1
+    }
+  },
+  RAxvprf: {
+    id: "RAxvprf",
+    prestige: 0,
+    type: "red",
+    costs: {
+      white: 3
+    }
+  },
+  ppnZugm: {
+    id: "ppnZugm",
+    prestige: 0,
+    type: "red",
+    costs: {
+      black: 3,
+      white: 1,
+      red: 1
+    }
+  },
+  aYwCrgb: {
+    id: "aYwCrgb",
+    prestige: 0,
+    type: "red",
+    costs: {
+      white: 2,
+      red: 2
+    }
+  },
+  oGAOcjx: {
+    id: "oGAOcjx",
+    prestige: 0,
+    type: "red",
+    costs: {
+      black: 2,
+      white: 2,
+      green: 1
+    }
+  },
+  GleWAsx: {
+    id: "GleWAsx",
+    prestige: 0,
+    type: "red",
+    costs: {
+      black: 1,
+      white: 2,
+      blue: 1,
+      green: 1
+    }
+  },
+  YwiobgN: {
+    id: "YwiobgN",
+    prestige: 1,
+    type: "blue",
+    costs: {
+      red: 4
+    }
+  },
+  klXoUpV: {
+    id: "klXoUpV",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      black: 2,
+      white: 1
+    }
+  },
+  UEtFRIF: {
+    id: "UEtFRIF",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      black: 1,
+      white: 1,
       green: 1,
-      blue: 1
+      red: 2
+    }
+  },
+  uDmkabC: {
+    id: "uDmkabC",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      black: 2,
+      green: 2
+    }
+  },
+  BTGmFjv: {
+    id: "BTGmFjv",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      black: 1,
+      white: 1,
+      green: 1,
+      red: 1
+    }
+  },
+  cWglpkC: {
+    id: "cWglpkC",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      white: 1,
+      green: 2,
+      red: 2
+    }
+  },
+  ClRYAYT: {
+    id: "ClRYAYT",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      black: 3
+    }
+  },
+  VGsaSpu: {
+    id: "VGsaSpu",
+    prestige: 0,
+    type: "blue",
+    costs: {
+      blue: 1,
+      green: 3,
+      red: 1
+    }
+  },
+  tOzhrOc: {
+    id: "tOzhrOc",
+    prestige: 0,
+    type: "black",
+    costs: {
+      green: 3
+    }
+  },
+  iigPmcI: {
+    id: "iigPmcI",
+    prestige: 0,
+    type: "black",
+    costs: {
+      white: 1,
+      blue: 2,
+      green: 1,
+      red: 1
+    }
+  },
+  Oyjniap: {
+    id: "Oyjniap",
+    prestige: 0,
+    type: "black",
+    costs: {
+      white: 2,
+      green: 2
+    }
+  },
+  WPTTnPo: {
+    id: "WPTTnPo",
+    prestige: 0,
+    type: "black",
+    costs: {
+      white: 1,
+      blue: 1,
+      green: 1,
+      red: 1
+    }
+  },
+  TCyGyuz: {
+    id: "TCyGyuz",
+    prestige: 1,
+    type: "black",
+    costs: {
+      blue: 4
+    }
+  },
+  COkuPCX: {
+    id: "COkuPCX",
+    prestige: 0,
+    type: "black",
+    costs: {
+      black: 1,
+      green: 1,
+      red: 3
+    }
+  },
+  AOrDBGH: {
+    id: "AOrDBGH",
+    prestige: 0,
+    type: "black",
+    costs: {
+      white: 2,
+      blue: 2,
+      red: 1
+    }
+  },
+  nVFalmB: {
+    id: "nVFalmB",
+    prestige: 0,
+    type: "black",
+    costs: {
+      green: 2,
+      red: 1
+    }
+  },
+  zYUxsBJ: {
+    id: "zYUxsBJ",
+    prestige: 1,
+    type: "white",
+    costs: {
+      white: 2,
+      blue: 3,
+      red: 3
+    }
+  },
+  OuwslfB: {
+    id: "OuwslfB",
+    prestige: 3,
+    type: "white",
+    costs: {
+      white: 6
+    }
+  },
+  RtkRfJA: {
+    id: "RtkRfJA",
+    prestige: 2,
+    type: "white",
+    costs: {
+      black: 3,
+      red: 5
+    }
+  },
+  SnJonze: {
+    id: "SnJonze",
+    prestige: 2,
+    type: "white",
+    costs: {
+      red: 5
+    }
+  },
+  gysMsZc: {
+    id: "gysMsZc",
+    prestige: 1,
+    type: "white",
+    costs: {
+      black: 2,
+      green: 3,
+      red: 2
+    }
+  },
+  jxwgtDq: {
+    id: "jxwgtDq",
+    prestige: 2,
+    type: "white",
+    costs: {
+      black: 2,
+      green: 1,
+      red: 4
+    }
+  },
+  fteyyQP: {
+    id: "fteyyQP",
+    prestige: 1,
+    type: "green",
+    costs: {
+      black: 2,
+      white: 2,
+      blue: 3
+    }
+  },
+  dNKGmyU: {
+    id: "dNKGmyU",
+    prestige: 2,
+    type: "green",
+    costs: {
+      green: 5
+    }
+  },
+  jamfsCZ: {
+    id: "jamfsCZ",
+    prestige: 1,
+    type: "green",
+    costs: {
+      white: 3,
+      green: 2,
+      red: 3
+    }
+  },
+  NgiKHEN: {
+    id: "NgiKHEN",
+    prestige: 2,
+    type: "green",
+    costs: {
+      blue: 5,
+      green: 3
+    }
+  },
+  DEmdmWD: {
+    id: "DEmdmWD",
+    prestige: 3,
+    type: "green",
+    costs: {
+      green: 6
+    }
+  },
+  ZEmRCDQ: {
+    id: "ZEmRCDQ",
+    prestige: 2,
+    type: "green",
+    costs: {
+      black: 1,
+      white: 4,
+      blue: 2
+    }
+  },
+  gHwrrnE: {
+    id: "gHwrrnE",
+    prestige: 2,
+    type: "red",
+    costs: {
+      black: 5,
+      white: 3
+    }
+  },
+  KPCSXue: {
+    id: "KPCSXue",
+    prestige: 1,
+    type: "red",
+    costs: {
+      black: 3,
+      blue: 3,
+      red: 2
+    }
+  },
+  lIvsCTn: {
+    id: "lIvsCTn",
+    prestige: 1,
+    type: "red",
+    costs: {
+      black: 3,
+      white: 2,
+      red: 2
+    }
+  },
+  gPGyQKz: {
+    id: "gPGyQKz",
+    prestige: 2,
+    type: "red",
+    costs: {
+      white: 1,
+      blue: 4,
+      green: 2
+    }
+  },
+  YwTLYaW: {
+    id: "YwTLYaW",
+    prestige: 3,
+    type: "red",
+    costs: {
+      red: 6
+    }
+  },
+  bqSrbRF: {
+    id: "bqSrbRF",
+    prestige: 2,
+    type: "blue",
+    costs: {
+      white: 5,
+      blue: 3
+    }
+  },
+  dlIKNmq: {
+    id: "dlIKNmq",
+    prestige: 2,
+    type: "blue",
+    costs: {
+      black: 4,
+      white: 2,
+      red: 1
+    }
+  },
+  qXYwMwc: {
+    id: "qXYwMwc",
+    prestige: 2,
+    type: "blue",
+    costs: {
+      blue: 5
+    }
+  },
+  AFSWqhB: {
+    id: "AFSWqhB",
+    prestige: 3,
+    type: "blue",
+    costs: {
+      blue: 6
+    }
+  },
+  RfZamFp: {
+    id: "RfZamFp",
+    prestige: 1,
+    type: "blue",
+    costs: {
+      black: 3,
+      blue: 2,
+      green: 3
+    }
+  },
+  mHfwdGx: {
+    id: "mHfwdGx",
+    prestige: 1,
+    type: "blue",
+    costs: {
+      blue: 2,
+      green: 2,
+      red: 3
+    }
+  },
+  bZExlNI: {
+    id: "bZExlNI",
+    prestige: 2,
+    type: "black",
+    costs: {
+      blue: 1,
+      green: 4,
+      red: 2
+    }
+  },
+  XeUZXjB: {
+    id: "XeUZXjB",
+    prestige: 3,
+    type: "black",
+    costs: {
+      black: 6
+    }
+  },
+  MAsTtxI: {
+    id: "MAsTtxI",
+    prestige: 1,
+    type: "black",
+    costs: {
+      black: 2,
+      white: 3,
+      green: 3
+    }
+  },
+  JYWfVrl: {
+    id: "JYWfVrl",
+    prestige: 2,
+    type: "black",
+    costs: {
+      green: 5,
+      red: 3
+    }
+  },
+  rdnHjqZ: {
+    id: "rdnHjqZ",
+    prestige: 1,
+    type: "black",
+    costs: {
+      white: 3,
+      blue: 2,
+      green: 2
+    }
+  },
+  cICMDmU: {
+    id: "cICMDmU",
+    prestige: 2,
+    type: "black",
+    costs: {
+      white: 5
+    }
+  },
+  zdweOXO: {
+    id: "zdweOXO",
+    prestige: 4,
+    type: "white",
+    costs: {
+      black: 6,
+      white: 3,
+      red: 3
+    }
+  },
+  iEXYTzX: {
+    id: "iEXYTzX",
+    prestige: 4,
+    type: "white",
+    costs: {
+      black: 7
+    }
+  },
+  cRvbLrY: {
+    id: "cRvbLrY",
+    prestige: 3,
+    type: "white",
+    costs: {
+      black: 3,
+      blue: 3,
+      green: 3,
+      red: 5
+    }
+  },
+  kccrTYk: {
+    id: "kccrTYk",
+    prestige: 5,
+    type: "white",
+    costs: {
+      black: 7,
+      white: 3
+    }
+  },
+  DFYqjXf: {
+    id: "DFYqjXf",
+    prestige: 4,
+    type: "green",
+    costs: {
+      white: 3,
+      blue: 6,
+      green: 3
+    }
+  },
+  IptQswg: {
+    id: "IptQswg",
+    prestige: 4,
+    type: "green",
+    costs: {
+      blue: 7
+    }
+  },
+  YMguFyb: {
+    id: "YMguFyb",
+    prestige: 3,
+    type: "green",
+    costs: {
+      black: 3,
+      white: 5,
+      blue: 3,
+      red: 3
+    }
+  },
+  XWkunAz: {
+    id: "XWkunAz",
+    prestige: 5,
+    type: "green",
+    costs: {
+      blue: 7,
+      green: 3
+    }
+  },
+  trxoTNU: {
+    id: "trxoTNU",
+    prestige: 4,
+    type: "red",
+    costs: {
+      blue: 3,
+      green: 6,
+      red: 3
+    }
+  },
+  MAbBqgr: {
+    id: "MAbBqgr",
+    prestige: 4,
+    type: "red",
+    costs: {
+      green: 7
+    }
+  },
+  fuhAlJU: {
+    id: "fuhAlJU",
+    prestige: 3,
+    type: "red",
+    costs: {
+      black: 3,
+      white: 3,
+      blue: 5,
+      green: 3
+    }
+  },
+  zOZHAvo: {
+    id: "zOZHAvo",
+    prestige: 5,
+    type: "red",
+    costs: {
+      green: 7,
+      red: 3
+    }
+  },
+  eppPvFT: {
+    id: "eppPvFT",
+    prestige: 4,
+    type: "blue",
+    costs: {
+      black: 3,
+      white: 6,
+      blue: 3
+    }
+  },
+  dQJtJTR: {
+    id: "dQJtJTR",
+    prestige: 4,
+    type: "blue",
+    costs: {
+      white: 7
+    }
+  },
+  dwMlQHh: {
+    id: "dwMlQHh",
+    prestige: 3,
+    type: "blue",
+    costs: {
+      black: 5,
+      white: 3,
+      green: 3,
+      red: 3
+    }
+  },
+  IUAwpVI: {
+    id: "IUAwpVI",
+    prestige: 5,
+    type: "blue",
+    costs: {
+      white: 7,
+      blue: 3
+    }
+  },
+  ErlDlly: {
+    id: "ErlDlly",
+    prestige: 4,
+    type: "black",
+    costs: {
+      black: 3,
+      green: 3,
+      red: 6
+    }
+  },
+  BzGGJre: {
+    id: "BzGGJre",
+    prestige: 4,
+    type: "black",
+    costs: {
+      red: 7
+    }
+  },
+  xxMFrSl: {
+    id: "xxMFrSl",
+    prestige: 3,
+    type: "black",
+    costs: {
+      white: 3,
+      blue: 3,
+      green: 5,
+      red: 3
+    }
+  },
+  nxEdFUx: {
+    id: "nxEdFUx",
+    prestige: 5,
+    type: "black",
+    costs: {
+      black: 3,
+      red: 7
     }
   }
 };
 
 export const nobles = {
-  1: {
-    id: "1",
+  BXCuxra: {
+    id: "BXCuxra",
+    prestige: 3,
+    costs: {
+      white: 3,
+      blue: 3,
+      black: 3
+    },
+    imgURL:
+      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  nrCSnNc: {
+    id: "nrCSnNc",
+    prestige: 3,
+    costs: {
+      blue: 3,
+      red: 3,
+      green: 3
+    },
+    imgURL:
+      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  wwKhBBz: {
+    id: "wwKhBBz",
+    prestige: 3,
+    costs: {
+      black: 3,
+      red: 3,
+      green: 3
+    },
+    imgURL:
+      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  KamJyHm: {
+    id: "KamJyHm",
+    prestige: 3,
+    costs: {
+      white: 3,
+      blue: 3,
+      green: 3
+    },
+    imgURL:
+      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  IhnmDAx: {
+    id: "IhnmDAx",
+    prestige: 3,
+    costs: {
+      white: 4,
+      blue: 4
+    },
+    imgURL:
+      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  UfaGOTy: {
+    id: "UfaGOTy",
+    prestige: 3,
+    costs: {
+      blue: 4,
+      green: 4
+    },
+    imgURL:
+      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
+  },
+  XVGShfJ: {
+    id: "XVGShfJ",
     prestige: 3,
     costs: {
       red: 4,
@@ -112,8 +913,8 @@ export const nobles = {
     imgURL:
       "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
   },
-  2: {
-    id: "2",
+  dsRtJSG: {
+    id: "dsRtJSG",
     prestige: 3,
     costs: {
       black: 4,
@@ -122,42 +923,23 @@ export const nobles = {
     imgURL:
       "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
   },
-  3: {
-    id: "3",
+  DmYYhBR: {
+    id: "DmYYhBR",
     prestige: 3,
     costs: {
-      red: 4,
-      green: 4
+      white: 3,
+      black: 3,
+      red: 3
     },
     imgURL:
       "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
   },
-  4: {
-    id: "4",
+  wJhwQDm: {
+    id: "wJhwQDm",
     prestige: 3,
     costs: {
-      black: 4,
-      red: 4
-    },
-    imgURL:
-      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
-  },
-  5: {
-    id: "5",
-    prestige: 3,
-    costs: {
-      red: 4,
-      green: 4
-    },
-    imgURL:
-      "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
-  },
-  6: {
-    id: "6",
-    prestige: 3,
-    costs: {
-      red: 4,
-      green: 4
+      white: 4,
+      black: 4
     },
     imgURL:
       "https://cf.geekdo-images.com/A_GXbAh-oYSAOGOVYFXPAHvfezU=/fit-in/1200x630/pic2803135.jpg"
