bd34043ca9de3a093a658a270ffd4a4bddb3b798
Bryan Chu <thisisbryanchu@gmail.com>
2018-03-10 16:56:07 -0500

    Adding counts to each Player's card stack
    Replacing map with reduce
    Removing unnecessary variable declaration

--- source/components/CardStack.js
+++ source/components/CardStack.js
@@ -5,20 +5,22 @@ import Card from "./Card";
 
 class CardStack extends Component {
   render() {
-    const type = this.props.type;
-
-    const cards = this.props.cards.map(card => {
-      if (card.type === type) {
-        return (
-          <div key={card.id} className="stack">
-            <Card key={card.id} id={card.id} />
-          </div>
-        );
-      }
-    });
+    const cards = this.props.cards.reduce(
+      (acc, card) =>
+        card.type === this.props.type
+          ? [
+              ...acc,
+              <div key={card.id} className="stack">
+                <Card key={card.id} id={card.id} />
+              </div>
+            ]
+          : acc,
+      []
+    );
 
     return (
       <div className="card__stack">
+        <div className="reminder__text centered">{cards.length}</div>
         <div className="groove">{cards}</div>
       </div>
     );
