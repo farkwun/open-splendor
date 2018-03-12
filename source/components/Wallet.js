0107253091ce88281b37f309df645d5c623166f8
Bryan Chu <thisisbryanchu@gmail.com>
2018-03-12 03:13:01 -0400

    Adding red reminder text if nearing max coins

--- source/components/Wallet.js
+++ source/components/Wallet.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 
 import { maxCoins } from "../helpers/Helpers";
 
+const WARN_OFFSET = 2;
+
 class Wallet extends Component {
   render() {
     const coins = Object.keys(this.props.coins).map(key => {
@@ -20,7 +22,14 @@ class Wallet extends Component {
     return (
       <div className="wallet">
         <h3 className="box__header">Wallet</h3>
-        <div className="reminder__text">
+        <div
+          className="reminder__text"
+          style={
+            numCoins < maxCoins - WARN_OFFSET
+              ? { color: "grey" }
+              : { color: "red" }
+          }
+        >
           <i>
             coins - {numCoins}/{maxCoins}
           </i>
