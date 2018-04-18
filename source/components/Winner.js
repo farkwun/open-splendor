06d5338b0d4c25b3df9eb8283c216746e9d3cad5
Bryan Chu <thisisbryanchu@gmail.com>
2018-04-18 01:56:35 -0400

    Tweaking Winner screen for better readability

--- source/components/Winner.js
+++ source/components/Winner.js
@@ -4,7 +4,7 @@ class Winner extends Component {
   render() {
     const players = Object.keys(this.props.players).map(id => (
       <h4>
-        {id} : {this.props.players[id].prestige} prestige |
+        {id} : {this.props.players[id].prestige} prestige |{" "}
         {this.props.players[id].cards.length} cards
       </h4>
     ));
@@ -12,13 +12,13 @@ class Winner extends Component {
       <div className="winner">
         <h2>Game Over!</h2>
         <h3>
-          {this.props.winner} has won the game with
-          {this.props.players[this.props.winner].prestige} prestige and
+          {this.props.winner} has won the game with{" "}
+          {this.props.players[this.props.winner].prestige} prestige and{" "}
           {this.props.players[this.props.winner].cards.length} development
           cards!
         </h3>
         <hr />
-        {players}}
+        {players}
       </div>
     );
   }
